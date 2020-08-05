a13bbfb9d2685cc49ddaacdd7fe567016e188e07
yang <yangseungmin82@gmail.com>
2020-08-05 15:02:05 +0900

    Hooks useReducer 추가

--- src/pages/Hooks.js
+++ src/pages/Hooks.js
@@ -1,44 +1,23 @@
-import React, { useState, useRef, useEffect, useMemo, useCallback } from "react";
-// import "./styles.css";
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+  useReducer,
+} from "react";
 
 function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
+  console.log(`활성 사용자 수를 세는중... : ${users.length}`);
+  return users.filter((user) => user.active).length;
 }
 
-function Hooks() {
-  let [light, setLight] = useState(0);
-
-  const setOff = () => setLight(0);
-  const setOn = () => setLight(1);
-
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: "",
     email: "",
-  });
-
-  const { username, email } = inputs;
-
-  // const onChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setInputs({
-  //     ...inputs,
-  //     [name]: value,
-  //   });
-  // };
-
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value
-      });
-    },
-    [inputs]
-  );
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: "velopert1",
@@ -47,7 +26,7 @@ function Hooks() {
     },
     {
       id: 2,
-      username: "myTester",
+      username: "tester",
       email: "tester@example.com",
       active: false,
     },
@@ -57,48 +36,91 @@ function Hooks() {
       email: "liz@example.com",
       active: false,
     },
-  ]);
+  ],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function Hooks() {
+  let [light, setLight] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { users } = state;
+  const { inputs } = state;
+  const { username, email } = inputs;
+
+  const setOff = () => setLight(0);
+  const setOn = () => setLight(1);
+
+  // reducer 사용
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value,
+    });
+  }, []);
 
   const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users.concat(user));
 
-    setInputs({
-      username: '',
-      email: ''
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: "CREATE_USER",
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
     });
     nextId.current += 1;
-  }, [users, username, email]);
+  }, [username, email]);
 
-  const onRemove = useCallback(
-    id => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함
-      setUsers(users.filter(user => user.id !== id));
-    },
-    [users]
-  );
-  const onToggle = useCallback(
-    id => {
-      setUsers(
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    },
-    [users]
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({
+      type: "TOGGLE_USER",
+      id,
+    });
+  }, []);
 
-  // const count = countActiveUsers(users);
-  const count = useMemo(
-    () => countActiveUsers(users),
-     [users]
-  );
+  const onRemove = useCallback((id) => {
+    dispatch({
+      type: "REMOVE_USER",
+      id,
+    });
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <div className="App">
@@ -115,7 +137,7 @@ function Hooks() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
       <div>활성사용자 수 : {count}</div>
     </div>
   );
@@ -127,8 +149,8 @@ function InputSample() {
     name: "",
     nickname: "",
   });
-  const nameInput = useRef();
 
+  const nameInput = useRef();
   const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
   const onChange = (e) => {
@@ -172,7 +194,7 @@ function InputSample() {
   );
 }
 
-const CreateUser = React.memo(({ username, email, onChange, onCreate })=>{
+const CreateUser = React.memo(({ username, email, onChange, onCreate }) => {
   return (
     <div>
       <input
@@ -190,14 +212,14 @@ const CreateUser = React.memo(({ username, email, onChange, onCreate })=>{
       <button onClick={onCreate}>등록</button>
     </div>
   );
-}); 
+});
 
 const User = React.memo(({ user, onRemove, onToggle }) => {
   useEffect(() => {
-    console.log('user 값이 설정됨');
+    console.log("user 값이 설정됨");
     console.log(user);
     return () => {
-      console.log('user 가 바뀌기 전..');
+      console.log("user 가 바뀌기 전..");
       console.log(user);
     };
   }, [user]);
@@ -222,9 +244,15 @@ const UserList = React.memo(({ users, onRemove, onToggle }) => {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
-})
+});
+
 export default Hooks;
